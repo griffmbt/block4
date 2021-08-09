92d90fe01865580efa4622f174a9cfe3fb8793d4
Renat Hi <renathi@MacBook-Pro-Renat.local>
2021-08-09 18:53:56 +0300

    для блока content: обработчик на кнопку

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1.2,
- 
+
   loop: true,
   pagination: {
     el: ".swiper-pagination",
@@ -60,18 +60,67 @@ function hideButtonEquipment() {
 }
 buttonEquipment.addEventListener("click", hideButtonEquipment);
 
+const contentBody = document.querySelector(".content__body");
+const hiddenContentText = contentBody.querySelector(".content__text--hidden");
+const buttonContent = contentBody.querySelector(".content__button");
+const buttonContentName = buttonContent.querySelector(".button__name");
+const buttonContentImg = buttonContent.querySelector(".content__button-img")
+
+const contentText768 = contentBody.querySelector(".content__768");
+const contentText320 = contentBody.querySelector(".content__320");
+
+function hideContent() {
+
+  if (hiddenContentText.style.display != "none") {
+    hiddenContentText.style.display = "none";
+    buttonContentName.textContent = 'Читать далее';
+    buttonContentImg.src = "./img/main/icon.svg";
+
+    if (window.innerWidth <= 768) {
+      contentText768.style.display = "none";
+    }
+    if (window.innerWidth <= 320) {
+      contentText320.style.display = "none";
+      contentText768.style.display = "none";
+    }
+  } else {
+    hiddenContentText.style.display = "block";
+    buttonContentName.textContent = 'Скрыть';
+    buttonContentImg.src = "./img/main/icon2.svg";
+
+    if (window.innerWidth <= 768) {
+      contentText768.style.display = "inline";
+    }
+    if (window.innerWidth <= 320) {
+      contentText320.style.display = "inline";
+      contentText768.style.display = "inline";
+    }
+  }
+}
+hideContent();
+buttonContent.addEventListener("click", hideContent);
+
+
 function hiddenItemIpad() {
   if (window.innerWidth <= 768) {
     equipmentItemIpad.classList.add("equipment__display--none");
     buttonHiddenIpad.forEach((button) => {
       button.classList.add("brands__display--none");
     });
+    contentText320.style.display = "inline";
+    contentText768.style.display = "none";
   }
   if (window.innerWidth > 768) {
     equipmentItemIpad.classList.remove("equipment__display--none");
     buttonHiddenIpad.forEach((button) => {
       button.classList.remove("brands__display--none");
     });
+    contentText320.style.display = "inline";
+    contentText768.style.display = "inline";
+  }
+  if (window.innerWidth <= 320) {
+    contentText320.style.display = "none";
+    contentText768.style.display = "none";
   }
 }
 
