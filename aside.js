7bd1457983728bb4c4ce3e2d4ffd10be90eba37f
Renat Hi <renathi@MacBook-Pro-Renat.local>
2021-08-10 21:02:30 +0300

    скрипт для aside

--- aside.js
+++ aside.js
@@ -1,12 +1,24 @@
-const burgerButton = document.querySelector('.header__button--burger');
-const aside = document.querySelector('.aside');
-const closeBurgerButton = document.querySelector('.button--burger-close');
+const burgerButton = document.querySelector(".header__button--burger");
+const aside = document.querySelector(".aside");
+const closeBurgerButton = document.querySelector(".button--burger-close");
 
-burgerButton.addEventListener('click', () => {
-    aside.classList.remove("not-visible");
-})
+const toggleAside = function () {
+  aside.classList.toggle("not-visible");
+};
 
-closeBurgerButton.addEventListener('click', () => {
-    aside.classList.add("not-visible");
-})
+burgerButton.addEventListener("click", (e) => {
+  //  e.stopPropagation();
+  toggleAside();
+});
 
+closeBurgerButton.addEventListener("click", (e) => {
+  //  e.stopPropagation();
+  toggleAside();
+});
+
+window.addEventListener("click", outsideClick);
+function outsideClick(e) {
+  if (e.target == aside) {
+    toggleAside();
+  }
+}
